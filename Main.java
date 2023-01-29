a5893b8bb4bb1a8bc5f865bbea35151930aeec2a
epsibaranis <epsibaranis@gmail.com>
2023-01-29 19:03:11 +0530

    Product of two number with default constructor

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Pof2nos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pof2nos a;
+		a=new Pof2nos();
+		a.find();
+		a.display();
+	}
+
+}
---
+++ Pof2nos.java
@@ -0,0 +1,16 @@
+package Pof2nos;
+
+public class Pof2nos {
+	private float x,y,p;
+	public Pof2nos() {
+		x=5;
+		y=7;
+	}
+	public void find() {
+		p=x*y;
+	}
+	public void display() {
+		System.out.println(p);
+		}
+
+}
